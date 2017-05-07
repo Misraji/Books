9d3255de841f4c1f9cae15e7ce51105a234646f3
Ashish Misra <amisra@salesforce.com>
2017-05-06 18:29:55 -0700

    Initial commit of Java HelloWord program. Tried to recreate the standard maven project structure. Will incorporate Maven at a later date.

---
+++ src/Core-Java-for-the-Impatient/src/main/java/ch01/sec01/HelloWorld.java
@@ -0,0 +1,8 @@
+package ch01.sec01;
+
+// Our first java program
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!!");
+	}
+}
---
+++ src/Core-Java-for-the-Impatient/src/main/java/ch01/sec01/MethodDemo.java
@@ -0,0 +1,13 @@
+package ch01.sec01;
+
+import java.util.Random;
+
+public class MethodDemo {
+
+	public static void main(String args[]) {
+		Random generator = new Random();
+
+		System.out.println(generator.nextInt());
+		System.out.println(generator.nextInt());
+	}
+}
